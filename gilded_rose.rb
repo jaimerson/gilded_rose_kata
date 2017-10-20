f74140133004b7dfe77b7991abb526ba3cb67649
James <jaimersonaraujo@gmail.com>
2017-10-20 19:25:16 -0300

    Create solution based on strategies pattern

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,48 +1,112 @@
+class BaseQualityUpdateStrategy
+  attr_reader :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def execute
+    update_quality
+    update_sell_in
+  end
+
+  protected
+
+  def decrease_quality(amount=quality_update_amount)
+    item.quality -= amount
+    item.quality = 0 if item.quality < 0
+  end
+
+  def increase_quality(amount=quality_update_amount)
+    item.quality += amount
+    item.quality = 50 if item.quality > 50
+  end
+
+  def update_quality
+  end
+
+  def update_sell_in
+    item.sell_in -= 1
+  end
+
+  def quality_update_amount
+    item.sell_in <= 0 ? 2 : 1
+  end
+end
+
+class DefaultStrategy < BaseQualityUpdateStrategy
+  def update_quality
+    decrease_quality
+  end
+end
+
+class BackstageConcertStrategy < BaseQualityUpdateStrategy
+  def update_quality
+    if item.sell_in <= 0
+      item.quality = 0
+    else
+      increase_quality(increase_amount)
+    end
+  end
+
+  def increase_amount
+    return 1 if item.sell_in > 10
+    item.sell_in <= 5 ? 3 : 2
+  end
+end
+
+class AgesWellStrategy < BaseQualityUpdateStrategy
+  def update_quality
+    increase_quality
+  end
+end
+
+class LegendaryStrategy < BaseQualityUpdateStrategy
+  def update_sell_in
+  end
+end
+
+class ConjuredStrategy < BaseQualityUpdateStrategy
+  def update_quality
+    decrease_quality
+  end
+
+  def quality_update_amount
+    super * 2
+  end
+end
+
+class ItemQualityUpdater
+  attr_reader :item, :strategy
+
+  def initialize(item)
+    @item = item
+    @strategy = strategy_for(item)
+  end
+
+  def update!
+    strategy.execute
+  end
+
+  private
+
+  STRATEGIES = {
+    'Backstage passes to a TAFKAL80ETC concert' => BackstageConcertStrategy,
+    'Aged Brie' => AgesWellStrategy,
+    'Sulfuras, Hand of Ragnaros' => LegendaryStrategy,
+    'Conjured Mana Cake' => ConjuredStrategy
+  }.freeze
+
+  def strategy_for(item)
+    STRATEGIES
+      .fetch(item.name, DefaultStrategy)
+      .new(item)
+  end
+end
+
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
-    end
+    ItemQualityUpdater.new(item).update!
   end
 end
 
